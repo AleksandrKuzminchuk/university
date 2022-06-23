6cfa2f470765092a23e7e9ed6690a09c8a7074e4
Mike_Chervinko <mchervinko@atlassian.com>
2022-06-24 00:34:51 +0300

    [MENTOR] DataSource+PropertyManager solution

--- src/main/java/ua/foxminded/task10/uml/configuration/SpringConfig.java
+++ src/main/java/ua/foxminded/task10/uml/configuration/SpringConfig.java
@@ -3,15 +3,14 @@ package ua.foxminded.task10.uml.configuration;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import ua.foxminded.task10.uml.exceptions.NotFoundException;
+import ua.foxminded.task10.uml.util.PropertyManager;
 
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 import javax.sql.DataSource;
+import java.io.File;
 import java.util.Properties;
 
 @Slf4j
@@ -20,39 +19,24 @@ import java.util.Properties;
 public class SpringConfig {
 
     @Bean
-    public DataSource dataSource() {
-        log.info("Create [InitialContext]->");
-        InitialContext cxt = null;
-        try {
-            cxt = new InitialContext();
-        } catch (NamingException e) {
-            throw new NotFoundException("InitialContext not found!", e);
-        }
-        log.info("Created [InitialContext] -> SUCCESSFULLY");
-
-        DataSource dataSource;
-        try {
-            log.info("Create [DataSource]->");
-            log.info("Create [FINDING... lookup -> connection to DB]->");
-            dataSource = (DataSource) cxt.lookup("java:/comp/env/jdbc/university");
-            log.info("FOUND lookup -> connection to DB -> SUCCESSFULLY");
-        } catch (NamingException e) {
-            throw new NotFoundException("lookup not found!", e);
-        }
-
-        if (dataSource == null) {
-            throw new NotFoundException("Data source not found!");
-        }
-        log.info("Created [DataSource] -> SUCCESSFULLY");
-        return dataSource;
+    public PropertyManager propertyManager() {
+        return new PropertyManager("resources" + File.separator + "application.properties");
     }
 
     @Bean
-    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
-        log.info("Create [jdbcTemplate]->");
-        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-        log.info("Created [JdbcTemplate] -> SUCCESSFULLY");
-        return jdbcTemplate;
+    public DataSource dataSource() {
+        log.info("Create [DataSource]->");
+        PropertyManager propertyManager = propertyManager();
+        String url = propertyManager.getProperty("url");
+        String username = propertyManager.getProperty("username");
+        String password = propertyManager.getProperty("password");
+        String driver = propertyManager.getProperty("driver");
+        boolean suppressClose = true;
+        SingleConnectionDataSource dataSource = new SingleConnectionDataSource(url, username, password, suppressClose);
+        dataSource.setSchema("public");
+        dataSource.setDriverClassName(driver);
+        log.info("Created [DataSource] -> SUCCESSFULLY");
+        return dataSource;
     }
 
     @Bean
@@ -61,28 +45,28 @@ public class SpringConfig {
         LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
         sessionFactory.setDataSource(dataSource());
         sessionFactory.setPackagesToScan("ua.foxminded.task10.uml");
+        log.info("Create [properties]->");
+        Properties properties = getProperties();
+        log.info("Created [properties] -> SUCCESSFULLY");
+        sessionFactory.setHibernateProperties(properties);
+        log.info("Created [sessionFactory] -> SUCCESSFULLY");
+        return sessionFactory;
+    }
 
-        log.info("Create [hibernateProperties]->");
+    private Properties getProperties() {
         Properties hibernateProperties = new Properties();
         hibernateProperties.getProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
         hibernateProperties.getProperty("hibernate.ejb.naming_strategy", "org.hibernate.cfg.ImprovedNamingStrategy");
         hibernateProperties.setProperty("hibernate.show_sql", "true");
         hibernateProperties.getProperty("hibernate.format_sql", "true");
-        log.info("Created [hibernateProperties] -> SUCCESSFULLY");
-
-        sessionFactory.setHibernateProperties(hibernateProperties);
-
-        log.info("Created [sessionFactory] -> SUCCESSFULLY");
-        return sessionFactory;
+        return hibernateProperties;
     }
 
     @Bean
     public HibernateTransactionManager transactionManager() {
         log.info("Create [transactionManager]->");
         HibernateTransactionManager transactionManager = new HibernateTransactionManager();
-
         transactionManager.setSessionFactory(sessionFactory().getObject());
-
         log.info("Created [transactionManager] -> SUCCESSFULLY");
         return transactionManager;
     }
