81036df9efff698243a84f72a7f7d9ad976ff69b
Mike_Chervinko <mchervinko@atlassian.com>
2022-07-02 01:53:20 +0300

    [MENTOR] configs optimized

--- src/main/java/ua/foxminded/task10/uml/configuration/JpaConfig.java
+++ src/main/java/ua/foxminded/task10/uml/configuration/JpaConfig.java
@@ -1,83 +1,13 @@
 package ua.foxminded.task10.uml.configuration;
 
-import lombok.AccessLevel;
-import lombok.RequiredArgsConstructor;
-import lombok.experimental.FieldDefaults;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
-import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.SingleConnectionDataSource;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import javax.sql.DataSource;
-import java.util.Properties;
-
-@Slf4j
 @Configuration
-@RequiredArgsConstructor
 @EnableTransactionManagement
 @PropertySource(value = "classpath:application.properties")
-@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 @EnableJpaRepositories(basePackages = "ua.foxminded.task10.uml.repository")
 public class JpaConfig {
-
-    private final Environment environment;
-
-    @Bean
-    public DataSource dataSource() {
-        log.info("Create [DataSource]->");
-        SingleConnectionDataSource dataSource = new SingleConnectionDataSource();
-        dataSource.setSchema("public");
-        dataSource.setDriverClassName(environment.getRequiredProperty("spring.datasource.driver-class-name"));
-        dataSource.setUrl(environment.getRequiredProperty("spring.datasource.url"));
-        dataSource.setUsername(environment.getRequiredProperty("spring.datasource.username"));
-        dataSource.setPassword(environment.getRequiredProperty("spring.datasource.password"));
-        dataSource.setSuppressClose(true);
-        log.info("Created [DataSource] -> SUCCESSFULLY");
-        return dataSource;
-    }
-
-    private Properties getProperties() {
-        Properties hibernateProperties = new Properties();
-        hibernateProperties.put("hibernate.dialect", environment.getRequiredProperty("spring.jpa.properties.hibernate.dialect"));
-        hibernateProperties.put("hibernate.ejb.naming_strategy", environment.getRequiredProperty("spring.jpa.hibernate.naming.implicit-strategy"));
-        hibernateProperties.put("hibernate.show_sql", environment.getRequiredProperty("spring.jpa.properties.hibernate.show_sql"));
-        hibernateProperties.put("hibernate.format_sql", environment.getRequiredProperty("spring.jpa.properties.hibernate.format_sql"));
-        hibernateProperties.put("current_session_context_class", environment.getRequiredProperty("spring.jpa.properties.hibernate.current_session_context_class"));
-        hibernateProperties.put("hibernate.temp.use_jdbc_metadata_defaults", environment.getRequiredProperty("spring.jpa.properties.hibernate.temp.use_jdbc_metadata_defaults"));
-        return hibernateProperties;
-    }
-
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(){
-        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
-        entityManagerFactoryBean.setDataSource(dataSource());
-        entityManagerFactoryBean.setPackagesToScan("ua.foxminded.task10.uml.model");
-
-        HibernateJpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
-        entityManagerFactoryBean.setJpaVendorAdapter(jpaVendorAdapter);
-        entityManagerFactoryBean.setJpaProperties(getProperties());
-
-        return entityManagerFactoryBean;
-    }
-
-    @Bean
-    public PlatformTransactionManager transactionManager(){
-        JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
-        return transactionManager;
-    }
-
-    @Bean
-    public PersistenceExceptionTranslationPostProcessor exceptionTranslationPostProcessor(){
-        return new PersistenceExceptionTranslationPostProcessor();
-    }
 }
