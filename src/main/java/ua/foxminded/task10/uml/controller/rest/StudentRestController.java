5e104df0662e470930b98e915906196e3718a3aa
Mike_vin <chervinkoms@gmail.com>
2022-07-26 01:18:27 +0300

    [MENTOR] Response example (+formatting)

--- src/main/java/ua/foxminded/task10/uml/controller/rest/StudentRestController.java
+++ src/main/java/ua/foxminded/task10/uml/controller/rest/StudentRestController.java
@@ -6,10 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import ua.foxminded.task10.uml.dto.GroupDTO;
 import ua.foxminded.task10.uml.dto.StudentDTO;
 import ua.foxminded.task10.uml.dto.response.StudentsResponse;
-import ua.foxminded.task10.uml.service.GroupService;
 import ua.foxminded.task10.uml.service.StudentService;
 
 import javax.validation.Valid;
@@ -23,28 +21,26 @@ import java.util.List;
 public class StudentRestController {
 
     private final StudentService studentService;
-    private final GroupService groupService;
-
-
 
     @GetMapping
-    public StudentsResponse findAll(){
+    public StudentsResponse findAll() {
         log.info("requested-> [GET]-'/api/students'");
         List<StudentDTO> studentsDTO = studentService.findAll();
         return new StudentsResponse(studentsDTO);
     }
 
     @PostMapping("/save")
-    public ResponseEntity<StudentDTO> save(@RequestBody @Valid StudentDTO studentDTO){
+    @ResponseStatus(HttpStatus.CREATED)
+    public StudentDTO save(@RequestBody @Valid StudentDTO studentDTO) {
         log.info("requested-> [POST]-'/api/students/save'");
         StudentDTO savedStudentDTO = studentService.save(studentDTO);
         log.info("SAVED {} SUCCESSFULLY", savedStudentDTO);
-        return new ResponseEntity<>(savedStudentDTO, HttpStatus.CREATED);
+        return savedStudentDTO;
     }
 
     @PatchMapping("/update/{id}")
     public ResponseEntity<HttpStatus> update(@RequestBody @Valid StudentDTO studentDTO,
-                                             @PathVariable("id") Integer id){
+                                             @PathVariable("id") Integer id) {
         log.info("requested-> [PATCH]-'/api/students/update/{id}'");
         studentDTO.setId(id);
         studentService.update(studentDTO);
@@ -53,7 +49,7 @@ public class StudentRestController {
     }
 
     @DeleteMapping("/{id}/delete")
-    public ResponseEntity<?> deleteById(@PathVariable("id") Integer id){
+    public ResponseEntity<?> deleteById(@PathVariable("id") Integer id) {
         log.info("requested-> [DELETE]-'/api/students/{id}/delete'");
         studentService.deleteById(id);
         log.info("DELETED STUDENT BY ID - {} SUCCESSFULLY", id);
@@ -61,7 +57,7 @@ public class StudentRestController {
     }
 
     @PatchMapping("/{id}/delete/from_group")
-    public ResponseEntity<?> deleteFromGroup(@PathVariable("id") Integer id){
+    public ResponseEntity<?> deleteFromGroup(@PathVariable("id") Integer id) {
         log.info("requested-> [PATCH]-'/api/students/{id}/delete/from_group'");
         studentService.deleteGroup(id);
         log.info("DELETED GROUP FROM STUDENT SUCCESSFULLY");
@@ -69,7 +65,7 @@ public class StudentRestController {
     }
 
     @DeleteMapping("/delete/all/by_group/")
-    public ResponseEntity<?> deleteAllByGroupId(@RequestBody StudentDTO studentDTO){
+    public ResponseEntity<?> deleteAllByGroupId(@RequestBody StudentDTO studentDTO) {
         log.info("requested-> [DELETE]-'/api/students/delete/all/by_group/{groupId}'");
         studentService.deleteByGroupId(studentDTO.getGroup().getId());
         log.info("DELETED STUDENTS BY GROUP ID - {} SUCCESSFULLY", studentDTO.getGroup().getId());
@@ -77,14 +73,14 @@ public class StudentRestController {
     }
 
     @GetMapping("/find/by_course/{course}")
-    public StudentsResponse findByCourseNumber(@PathVariable("course") Integer course){
+    public StudentsResponse findByCourseNumber(@PathVariable("course") Integer course) {
         log.info("requested-> [GET]-'/api/students/found/by_course/{course}'");
         List<StudentDTO> studentsDTO = studentService.findByCourseNumber(course);
         return new StudentsResponse(studentsDTO);
     }
 
     @DeleteMapping("/delete/by_course/{course}")
-    public ResponseEntity<?> deleteAllByCourse(@PathVariable("course") Integer course){
+    public ResponseEntity<?> deleteAllByCourse(@PathVariable("course") Integer course) {
         log.info("requested-> [DELETE]-'/api/students/delete/by_course/{course}'");
         studentService.deleteByCourseNumber(course);
         log.info("DELETED STUDENTS BY COURSE - {} SUCCESSFULLY", course);
@@ -92,21 +88,21 @@ public class StudentRestController {
     }
 
     @GetMapping("/find/by_group/{groupId}")
-    public StudentsResponse findByGroupId(@PathVariable("groupId") Integer groupId){
+    public StudentsResponse findByGroupId(@PathVariable("groupId") Integer groupId) {
         log.info("requested-> [GET]-'/api/students/found/by_group/{groupId}'");
         List<StudentDTO> studentsDTO = studentService.findByGroupId(groupId);
         return new StudentsResponse(studentsDTO);
     }
 
     @GetMapping("/find/by_name_surname")
-    public StudentsResponse findByNameOrSurname(@RequestBody StudentDTO studentDTO){
+    public StudentsResponse findByNameOrSurname(@RequestBody StudentDTO studentDTO) {
         log.info("requested-> [GET]-'/api/students/find/by_name_surname'");
         List<StudentDTO> students = studentService.findByNameOrSurname(studentDTO.getFirstName(), studentDTO.getLastName());
         return new StudentsResponse(students);
     }
 
     @DeleteMapping("/delete/all")
-    public ResponseEntity<?> deleteAll(){
+    public ResponseEntity<?> deleteAll() {
         log.info("requested- [DELETE]-'/api/students/delete/all'");
         studentService.deleteAll();
         log.info("DELETED ALL SUCCESSFULLY");
