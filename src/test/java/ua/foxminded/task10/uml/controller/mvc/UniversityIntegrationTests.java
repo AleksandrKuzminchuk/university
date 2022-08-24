b99ca03e0f9005ea42a8ec158231d7052333a895
Aleksandr Kuzminchuk <lgoptimusg252@gmail.com>
2022-08-24 14:31:37 +0300

    added integration test mvc university, templates to resources and some changes

--- src/test/java/ua/foxminded/task10/uml/controller/mvc/UniversityIntegrationTests.java
+++ src/test/java/ua/foxminded/task10/uml/controller/mvc/UniversityIntegrationTests.java
@@ -1,4 +1,4 @@
-package ua.foxminded.task10.uml.controller.rest;
+package ua.foxminded.task10.uml.controller.mvc;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -6,31 +6,31 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import ua.foxminded.task10.uml.dto.*;
 import ua.foxminded.task10.uml.service.*;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static ua.foxminded.task10.uml.util.formatters.DateTimeFormat.formatter;
+import static ua.foxminded.task10.uml.util.MediaTypeTest.MEDIA_TYPE;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 @Sql(value = {"classpath:create-table-classrooms.sql", "classpath:create-table-subjects.sql", "classpath:create-table-teachers_subjects.sql",
         "classpath:create-table-teachers.sql", "classpath:create-table-groups.sql", "classpath:create-table-events.sql"},
         executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
-class UniversityRestControllerIntegrationTest {
+class UniversityIntegrationTests {
 
     @Autowired
     private EventService service;
@@ -64,16 +64,37 @@ class UniversityRestControllerIntegrationTest {
     }
 
     @Test
-    void givenEventsDTOList_whenShoeUniversity_thenReturnEventsDTOList() throws Exception {
+    void shouldReturnEventsDTOList_whenShowUniversity() throws Exception {
 
-        createEventsDTO();
+        List<EventDTO> eventsDTO = createEventsDTO();
 
-        ResultActions response = mockMvc.perform(get("/api/university"));
+        ResultActions response = mockMvc.perform(get("/university"));
 
         response.andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.events.size()", is(2)));
+                .andExpect(content().contentType(MEDIA_TYPE))
+                .andExpect(model().attribute("events", hasSize(2)))
+                .andExpect(model().attribute("events", hasItem(
+                        allOf(
+                                hasProperty("id", is(1)),
+                                hasProperty("dateTime", is(eventsDTO.get(0).getDateTime())),
+                                hasProperty("group", is(eventsDTO.get(0).getGroup())),
+                                hasProperty("classroom", is(eventsDTO.get(0).getClassroom())),
+                                hasProperty("subject", is(eventsDTO.get(0).getSubject())),
+                                hasProperty("teacher", is(eventsDTO.get(0).getTeacher()))
+                        )
+                )))
+                .andExpect(model().attribute("events", hasItem(
+                        allOf(
+                                hasProperty("id", is(2)),
+                                hasProperty("dateTime", is(eventsDTO.get(1).getDateTime())),
+                                hasProperty("group", is(eventsDTO.get(1).getGroup())),
+                                hasProperty("classroom", is(eventsDTO.get(1).getClassroom())),
+                                hasProperty("subject", is(eventsDTO.get(1).getSubject())),
+                                hasProperty("teacher", is(eventsDTO.get(1).getTeacher()))
+                        )
+                )))
+                .andExpect(model().attribute("count", is(2)));
     }
 
     private SubjectDTO createSubjectDTO(){
@@ -105,6 +126,8 @@ class UniversityRestControllerIntegrationTest {
         ClassroomDTO classroomDTO = createClassroomDTO();
         GroupDTO groupDTO = createGroupDTO();
         TeacherDTO teacherDTO = createTeacherDTO();
+        ClassroomDTO classroomDTO1 = new ClassroomDTO();
+        classroomDTO1.setNumber(78);
         GroupDTO groupDTO1 = new GroupDTO();
         groupDTO1.setName("G-67");
         eventsDTO.add(new EventDTO(LocalDateTime.now(), subjectDTO,
